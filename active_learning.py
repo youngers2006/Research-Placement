27e1513ef3189a3567e0e2af717f5db94af27e82
youngers2006 <sy2224@ic.ac.uk>
2025-09-10 18:49:53 +0100

    added

--- active_learning.py
+++ active_learning.py
@@ -8,17 +8,27 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from typing import Any
 import jraph
+import optax
 from dataclasses import dataclass
 
 class ActiveLearning:
-    def __init__(self, seen_boundary_displacements, confidence_bound, optimiser, learn_rate, epochs, alpha, gamma):
+    def __init__(self, seen_boundary_displacements, confidence_bound, Model, learn_rate, epochs, alpha, gamma):
+        self.Model = Model
         self.seen_bds = seen_boundary_displacements
         self.bound = confidence_bound
         self.LR = learn_rate
         self.epochs = epochs
         self.alpha = alpha
         self.gamma = gamma
-        self.optimiser = optimiser
+        self.optimiser =  nnx.Optimizer(
+                                self.Model,
+                                optax.adam(
+                                    learning_rate=learn_rate, 
+                                    b1=0.999, 
+                                    b2=0.9
+                                ),
+                            wrt=nnx.Param
+                        )
 
     def check_distance(self, current_displacement) -> bool:
         diff = self.seen_bds - current_displacement
@@ -27,11 +37,11 @@ class ActiveLearning:
         should_query = (closest_vector_sq < self.bound)
         return should_query
     
-    def create_graph(self, boundary_displacements, Model):
-        boundary_nodes = Model.boundary_nodes
-        nodes = Model.base_graph.nodes
+    def create_graph(self, boundary_displacements):
+        boundary_nodes = self.Model.boundary_nodes
+        nodes = self.Model.base_graph.nodes
         new_nodes = nodes.at[boundary_nodes].set(boundary_displacements)
-        graph = self.base_graph.replace(nodes=new_nodes)
+        graph = self.Model.base_graph.replace(nodes=new_nodes)
         return graph
     
     def loss_fn(self, target_e, target_e_prime, e_pred, e_prime_pred):
@@ -40,7 +50,7 @@ class ActiveLearning:
         return (self.alpha * loss_e + self.gamma * loss_e_prime)
     
     @nnx.jit
-    def train_step(self, Model, target_e, target_e_prime, graph):
+    def train_step(self, target_e, target_e_prime, graph):
         def wrapped_loss(Model):
             e_pred, e_prime_pred = Model.call_single(graph)
             loss = self.loss_fn(
@@ -51,13 +61,13 @@ class ActiveLearning:
             )
             return loss
     
-        grads = nnx.grad(wrapped_loss, argnums=0)(Model)
-        self.optimiser.update(Model, grads)
+        grads = nnx.grad(wrapped_loss, argnums=0)(self.Model)
+        self.optimiser.update(self.Model, grads)
     
-    def Learn(self, Model, current_displacement_graph, target_e_from_sim, target_e_prime_from_sim):
+    def Learn(self, current_displacement_graph, target_e_from_sim, target_e_prime_from_sim):
         for _ in range(self.epochs):
             self.train_step(
-                Model, 
+                self.Model, 
                 target_e_from_sim, 
                 target_e_prime_from_sim, 
                 current_displacement_graph
@@ -69,34 +79,15 @@ class ActiveLearning:
         e_prime = 0
         return e, e_prime
         
-    def __call__(self, Model, current_displacement):
+    def __call__(self, current_displacement):
         should_query = self.check_distance(current_displacement)
         current_displacement_graph = self.create_graph(current_displacement)
         if should_query:
             e_sim, e_prime_sim = self.query_fem(current_displacement)
-            self.Learn(Model, current_displacement, e_sim, e_prime_sim)
+            self.Learn(self.Model, current_displacement, e_sim, e_prime_sim)
             return e_sim, e_prime_sim
         else:
-            e_scaled, e_prime_scaled = Model.call_single(current_displacement_graph)
-            e, e_prime = Model.unscale_predictions(e_scaled, e_prime_scaled)
+            e_scaled, e_prime_scaled = self.Model.call_single(current_displacement_graph)
+            e, e_prime = self.Model.unscale_predictions(e_scaled, e_prime_scaled)
             return e, e_prime
-        
 
-
-def train_step(Model, optimiser, graph_batch, Target_batch, batched_zero_graph, *, alpha, gamma, lambda_):
-
-    def wrapped_loss(Model):
-        loss = loss_fn(
-            graph_batch,
-            Target_batch,
-            batched_zero_graph,
-            Model=Model,
-            alpha=alpha,
-            gamma=gamma,
-            lam=lambda_ 
-        )
-        return loss
-    
-    loss, grads = nnx.value_and_grad(wrapped_loss, argnums=0)(Model)
-    optimiser.update(Model, grads)
-    return loss
