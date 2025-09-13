00c6b800099834ae62321d048bcfc845ecde0649
youngers2006 <sy2224@ic.ac.uk>
2025-09-13 23:37:15 +0100

    added

--- active_learning.py
+++ active_learning.py
@@ -15,6 +15,20 @@ from JAX_FEM_active_learning import Run_Sim
 from ProjectUtils import restitch
 
 class ActiveLearningModel:
+    """
+    Desc: For a set of displacements found in the higher level simulation, active learning will decide, based on the geometric distance between 
+        the displacement vector it is looking at and all displacement vectors seen by the model, whether the models prediction can be trusted and therfore output
+        or if the model is not confident in its prediction, in which case it will query an FEM solver and train on the result. Over time the model should see many
+        displacement vectors and so should not have to query often. This active learning approach uses a vectorised method meaning that the model will not have the 
+        benefit of having seen any similar vectors that may reside in the same prediction step, this will lead to a greater number of simulations needing to be run
+        but benefits from all steps being done in parrallel.
+    Notes: Class functions are vectorised and jit compiled. For this active learning approach to function effectively the model must be 'kick-started' with
+        a more general dataset to allow the model to only finetune its predictions during time sensitive deployment of this model. To achieve maximum performance the 
+        mesh used must be of equal size throughout all samples it sees during deployment as to maximise the effect of XLA jit compilation.
+    Input: 1. Array of all diaplacement vectors in the current prediction step: jax.Array, 2. Mesh of elements being analysed.
+    Output: 1. Energy prediction array, 2. Energy sensitivity prediction array.
+    Secondary effects: Model will be trained on any data outside a defined confidence bound, data will be generated via a FEM simulation and added to the datastore. 
+    """
     def __init__(self, seen_boundary_displacements, confidence_bound, Model, optimiser, learn_rate, epochs, alpha, gamma):
         self.Model = Model
         self.seen_bds = seen_boundary_displacements
@@ -43,11 +57,13 @@ class ActiveLearningModel:
         return should_query_batch
     
     def query_or_not(self, query_array):
+        "Returns an array of indicies which correspond to displacement vectors that should and shouldnt be queried"
         should_query = jnp.where(query_array)[0]
         not_query = jnp.where(~query_array)[0]
         return should_query, not_query
     
     def create_graphs(self, boundary_displacements: jax.Array) -> list:
+        "Creates a list of jraph graphs that can be analysed by the Model"
         graphs = []
         boundary_nodes = self.Model.boundary_nodes
         base_nodes = self.Model.base_graph.nodes
@@ -63,12 +79,14 @@ class ActiveLearningModel:
         return graphs
     
     def loss_fn(self, target_e_batch, target_e_prime_batch, e_pred_batch, e_prime_pred_batch):
+        "Defined loss function: uses MSE for both the energy and energy sensitivity"
         loss_e = jnp.mean((e_pred_batch - target_e_batch)**2)
         loss_e_prime = jnp.mean((e_prime_pred_batch - target_e_prime_batch)**2)
         return (self.alpha * loss_e + self.gamma * loss_e_prime)
     
     @nnx.jit
     def train_step(self, target_e_batch, target_e_prime_batch, graphs_batch):
+        "Defines one step in which a batch is processed and the model weights are updated, jit compiled for speed"
         def wrapped_loss(Model):
             e_pred_batch, e_prime_pred_batch = Model(graphs_batch)
             loss = self.loss_fn(
@@ -83,6 +101,7 @@ class ActiveLearningModel:
         self.optimiser.update(self.Model, grads)
     
     def Learn(self, applied_displacement_graphs_list: jraph.GraphsTuple, target_e_from_sim, target_e_prime_from_sim):
+        "Iterates trainstep for a defined number of steps"
         for _ in tqdm(range(self.epochs), desc="Training model on simulation data", leave=False):
             self.train_step(
                 self.Model, 
@@ -91,8 +110,8 @@ class ActiveLearningModel:
                 applied_displacement_graphs_list
             )
     
-    def query_fem(self, applied_displacements, Mesh): # displacement batch (batch_size, num_nodes, 3)
-        """calls a jax_fem sim function from another file"""
+    def query_fem(self, applied_displacements, Mesh): 
+        """calls a jax_fem simulation when queried by the model"""
         vmapped_sim_fn = jax.vmap(fun=Run_Sim, in_axes=(None, None, 0))
         e_batch, e_prime_batch = vmapped_sim_fn(
             Mesh, 
@@ -102,6 +121,11 @@ class ActiveLearningModel:
         return e_batch, e_prime_batch
         
     def __call__(self, applied_displacements: jax.Array, Mesh) -> jax.Array:
+        """
+        Main call: queries the FEM solver for all samples outside the geometric confidence bound and trains on the data,
+            and all samples within the confidence bound are processed by the model. The predictions from the Model and simulation
+            are then stitched together and returned.
+        """
         should_query = self.check_distance(applied_displacements)
         applied_displacement_graphs_list = self.create_graphs(applied_displacements) 
         query_idx, confident_idx = self.query_or_not(should_query)
